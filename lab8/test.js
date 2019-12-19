ef47369500aaa78a97916f2e9151295902829175
Dmitry <ryazanovpr@gmail.com>
2019-12-19 16:52:29 +0300

    develop lab 8

--- lab8/test.js
+++ lab8/test.js
@@ -1,25 +1,23 @@
 require('chromedriver');
 const assert = require('assert');
 const {Builder, Key, By, until} = require('selenium-webdriver');
+const config = require('./config/testConfig.js');
 
 describe('Checkout Google.com', function () {
     let driver;
 
     before(async function() {
-        driver = await new Builder().forBrowser('chrome').build();
+        driver = await new Builder().forBrowser(config.browser).build();
     });
 
     it('Search on Google', async function() {
         await driver.get('http://52.136.215.164:9000/');
-        await driver.findElement(By.name('q')).click();
-        await driver.findElement(By.name('q')).sendKeys('dalenguyen', Key.RETURN);
-        // await driver.wait(until.titleIs('dalenguyen - Google Search'), 1000);
-        await driver.wait(until.elementLocated(By.id('rcnt')), 10000);
-        await driver.findElement(By.name('q')).clear();
-
-        let title = await driver.getTitle();
-        assert.equal(title, 'dalenguyen - Google Search');              
+        await driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div/div[2]/a")).click();
+        await driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div/div[2]/ul/li[1]/a")).click();
+        await driver.findElement(By.css("input#login")).sendKeys(config.userLogin);
+        await driver.findElement(By.css("input#pasword")).sendKeys(config.userPassword);
+        await driver.findElement(By.css("#login > button")).click();
     });
 
     after(() => driver && driver.quit());
-})
+});
